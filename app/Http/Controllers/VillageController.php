34ff6ada78da5a4333fb4855b1de3460c4a4c0bb
Hapid Fadli <hapidzfadli@gmail.com>
2023-07-11 17:45:05 +0700

    test

---
+++ app/Http/Controllers/VillageController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Village;
+use Illuminate\Http\Request;
+
+class VillageController extends Controller
+{
+    public function index()
+    {
+        // Tampilkan daftar desa
+        $villages = Village::all();
+        return response()->json($villages);
+    }
+
+    public function create()
+    {
+        // Tampilkan form untuk membuat desa baru
+        // Anda dapat menyesuaikan logika ini sesuai dengan kebutuhan Anda
+        return response()->json('Create form');
+    }
+
+    public function store(Request $request)
+    {
+        // Simpan desa baru ke dalam basis data
+        Village::create($request->all());
+        return response()->json('Village created', 201);
+    }
+
+    public function show($id)
+    {
+        // Tampilkan detail desa berdasarkan ID
+        $village = Village::findOrFail($id);
+        return response()->json($village);
+    }
+
+    public function edit($id)
+    {
+        // Tampilkan form untuk mengedit desa berdasarkan ID
+        // Anda dapat menyesuaikan logika ini sesuai dengan kebutuhan Anda
+        return response()->json('Edit form');
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Update desa berdasarkan ID
+        $village = Village::findOrFail($id);
+        $village->update($request->all());
+        return response()->json('Village updated');
+    }
+
+    public function destroy($id)
+    {
+        // Hapus desa berdasarkan ID
+        $village = Village::findOrFail($id);
+        $village->delete();
+        return response()->json('Village deleted');
+    }
+}
